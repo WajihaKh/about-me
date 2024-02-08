39b0bdbd6e6fb2042b138304a91ba9aaeb6d2933
Wajiha Khan <jiya.r.kh@gmail.com>
2024-02-08 00:36:44 -0500

    Added more else if statements and prompts

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,16 @@
-<script>
+let name = prompt("Hello, what is your name?", "");
+document.write(" It is a pleasure meeting you ", name)
 
-    let cats = prompt ("How many cats do you have?");
-    document.write("You have", "cats!");
+if (confirm("Are you ready for work?")) {
+    document.write(" LETS GO!");
+} else {
+    document.write(" GOODNIGHT");
+}
 
+if (confirm("Have you ever walked your cat?")) {
+    document.write(" Awesome training!");
+} else {
+    console.log("It's okay! Mine did not like the leash either!");
+}
 
-</script>
+document.write(" ଘ(੭ˊᵕˋ)੭");
