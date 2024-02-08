4e81dbf6dcf7bb2cc90bdb3a968b021ea1a10211
Wajiha Khan <jiya.r.kh@gmail.com>
2024-02-08 15:40:38 -0500

    Added functions

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,26 @@
-let name = prompt("Hello, what is your name?", "");
-document.write(" It is a pleasure meeting you ", name)
+
+function nameOfPerson() {
+let name = prompt("Hello, what is your name?" );
+document.write(" It is a pleasure meeting you " + name)
+}
+
+
+function questionOne() {
 
 if (confirm("Are you ready for work?")) {
     document.write(" LETS GO!");
 } else {
-    document.write(" GOODNIGHT");
+    alert(" GOODNIGHT");
+}
 }
 
+function questionTwo() {
 if (confirm("Have you ever walked your cat?")) {
     document.write(" Awesome training!");
 } else {
-    console.log("It's okay! Mine did not like the leash either!");
+    alert("It's okay! Mine did not like the leash either!");
+}
 }
 
-document.write(" ଘ(੭ˊᵕˋ)੭");
+
+document.write(" ଘ(੭ˊᵕˋ)੭ ");
