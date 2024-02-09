ee61d255a58ba38e3b2ea0a6007152cc49a2b6cf
Wajiha Khan <jiya.r.kh@gmail.com>
2024-02-09 01:42:52 -0500

    added while loop

--- js/javascript.js
+++ js/javascript.js
@@ -5,6 +5,26 @@ document.write(" It is a pleasure meeting you " + name)
 }
 
 
+function howIsYourDay() {
+
+    let emotions = '';
+    let good = true;
+    let alright = true;
+    let great = true;
+
+    while ( emotions != good || alright || great ) {
+        emotions= prompt ("How is your day going?");
+        if ( emotions != good || alright || great ) {
+        alert("Sorry you're not feeling well, come back later!");
+
+    } else {
+        alert("That's good to hear!")
+    }
+}
+    document.write("Enjoy the website!");
+}
+
+
 function questionOne() {
 
 if (confirm("Are you ready for work?")) {
@@ -23,4 +43,9 @@ if (confirm("Have you ever walked your cat?")) {
 }
 
 
+
+
 document.write(" ଘ(੭ˊᵕˋ)੭ ");
+
+// One while loop 
+// One loop with an emoji
